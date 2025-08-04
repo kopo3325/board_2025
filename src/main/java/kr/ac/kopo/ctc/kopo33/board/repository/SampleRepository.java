4de1f49e4be9da9aa5168347a6a27e86f1ef9fa7
DESKTOP-PMID8VC\USER <wpdnrwpdnr@gmail.com>
2025-08-04 15:31:38 +0900

    jpa 활용

--- src/main/java/kr/ac/kopo/ctc/kopo33/board/repository/SampleRepository.java
+++ src/main/java/kr/ac/kopo/ctc/kopo33/board/repository/SampleRepository.java
@@ -1,7 +1,17 @@
 package kr.ac.kopo.ctc.kopo33.board.repository;
 
 import kr.ac.kopo.ctc.kopo33.board.domain.Sample;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface SampleRepository extends JpaRepository<Sample, Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface SampleRepository extends JpaRepository<Sample, Long>, JpaSpecificationExecutor<Sample> {
+    Optional<Sample> findOnByTitle(String title);
+    Page<Sample> findAllByTitle(String type, Pageable pageable);
+    List<Sample> findAllByTitle(String type);
+    List<Sample> findByTitleLike(String type);
 }
--- src/test/java/kr/ac/kopo/ctc/kopo33/board/repository/SampleRepositoryTest.java
+++ src/test/java/kr/ac/kopo/ctc/kopo33/board/repository/SampleRepositoryTest.java
@@ -4,8 +4,12 @@ import kr.ac.kopo.ctc.kopo33.board.domain.Sample;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,8 +20,25 @@ public class SampleRepositoryTest {
     SampleRepository sampleRepository;
 
     @Test
-    void findAll() {
-        List<Sample> list = sampleRepository.findAll();
-        assertEquals(2, list.size());
+    void findAllByTitle() {
+        Map<String, Object> filter = new HashMap<>();
+        filter.put("title", "t1");
+
+        PageRequest pageable = PageRequest.of(0,10);
+        Page<Sample> page = sampleRepository.findAll(SampleSpecs.search(filter), pageable);
+
+        for (Sample s : page) {
+            System.out.println(s.getTitle());
+        }
+    }
+
+    @Test
+    void findByTitleLike() {
+        List<Sample> list = sampleRepository.findByTitleLike("%t1%");
+
+        for (Sample s : list) {
+            System.out.println(s.getTitle());
+        }
+
     }
 }
